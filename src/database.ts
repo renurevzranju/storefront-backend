dc2aaf39abcce9b2eaeb6c8afc4c6d079c47d152
Revathi <revathimeenashi@gmail.com>
2023-06-10 18:15:37 +0530

    Added database.ts and updated the readme file

---
+++ src/database.ts
@@ -0,0 +1,24 @@
+import { Pool } from 'pg'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const {
+  POSTGRES_HOST,
+  POSTGRES_DB,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_TEST_DB,
+  ENV,
+} = process.env;
+
+const databaseName : string | undefined = (ENV || '').trim() === 'test' ? POSTGRES_TEST_DB : POSTGRES_DB;
+
+const client = new Pool({
+    host: POSTGRES_HOST,
+    database: databaseName,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+});
+
+export default client;
